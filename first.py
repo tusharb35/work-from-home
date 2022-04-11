fa0b878400eaef456e46d802501ce23e056256f2
Tushar <computerhp80tushar@gmail.com>
2022-04-11 23:33:50 +0530

    4th commit

--- first.py
+++ first.py
@@ -3,10 +3,12 @@ mycon=conn.connect('project.db')
 
 cursor=mycon.cursor()
 
-#cursor.execute("select curdate()")
-#date=cursor.fetchall()
+st="select curdate()"
 
-#print("TODAY DATE IS ",date)
+cursor.execute(st)
+date=cursor.fetchall()
+
+print("TODAY DATE IS ",date)
 
 
 #===========Creating table for the operations======
@@ -14,31 +16,41 @@ cursor=mycon.cursor()
 cursor.execute("CREATE TABLE if not exists student1(ADMNO int(4) primary key ,NAME varchar(30) not null,CLASS int(2) not null,SEC char(1) not null,ROLLNO int(2))")
 
 cursor.execute("CREATE TABLE if not exists student2(ADMNO int(4) primary key,ATTENDENCE int(11))")
+
+
+
+#==================MAIN PROGRAM====================
+
+
+print('''
+          
+          _____________________________________________________
+                    MENU
+          ----------------------------------------------------- 
+                  
+             1.ATTENDACE
+             
+             2.ADDING NEW STUDENT (NAME/CLASS/SECTION/ADMISSION 
+               NUMBER FOLLOWING DATA SHOULD ENTER TOGETHER)
+             
+             3.UPDATING NAME
+             
+             4.UPDATING CLASS
+             
+             5.UPDATING SECTION
+             
+             6.UPDATING ADMISSION NUMBER
+             
+             7.TO DISPLAY ATTENDENCE OF A PARTICULAR STUDENT 
+               (ADMN.IS REQUIRED)
+               
+             8.DELETING DATA OF STUDENT
+             
+             9.TO DISPLAY ALL DATA
+             
+             10.EXIT
+           _____________________________________________________  ''')
              
              
 ch=eval(input("ENTER THE NUMBER OF OPERATION YOU WANT TO DO :- "))
 
-#================2.ADDING NEW STUDENT=================
-
-cursor.execute("select * from student1")
-data=cursor.fetchall()
-for a in data:
-    print(a)
-cursor.execute("select * from student2")
-data1=cursor.fetchall()
-for a in data1:
-    print(a)
-
-
-if ch==7:
-    print('''
-             TO DISPLAY ATTENDENCE OF A PARICULAR STUDENT  ''')
-    name=input('ENTER THE NAME OF THE STUDENT :- ')
-    clas=eval(input('ENTER THE CLASS OF THE STUDENT :- '))
-    sec=input('ENTER THE SECTION OF THE STUDENT :- ')
-    admo=eval(input('ENTER THE ADMISSION NUMBER OF THE STUDENT :- '))
-    st="select rollno,name,attendence,ADMNO from student1,student2 where name='{}' and class={} and sec='{}' and student1.ADMNO=student2.ADMNO".format(name,clas,sec)
-    cursor.execute(st)
-    date=cursor.fetchall()
-    for i in data:
-        print(i)
--- test.py
+++ test.py
@@ -3,3 +3,58 @@ mycon=conn.connect(host='localhost', user='root', passwd='tushar', database='pro
 
 if mycon.is_connected():
     print("successfully connected to mysql database")
+
+
+cursor=mycon.cursor()
+
+st="select curdate()"
+
+cursor.execute(st)
+date=cursor.fetchall()
+
+print("TODAY DATE IS ",date)
+
+
+#===========Creating table for the operations======
+
+cursor.execute("CREATE TABLE if not exists student1(ADMNO int(4) primary key ,NAME varchar(30) not null,CLASS int(2) not null,SEC char(1) not null,ROLLNO int(2))")
+
+cursor.execute("CREATE TABLE if not exists student2(ADMNO int(4) primary key,ATTENDENCE int(11))")
+
+
+
+#==================MAIN PROGRAM====================
+
+
+print('''
+          
+          _____________________________________________________
+                    MENU
+          ----------------------------------------------------- 
+                  
+             1.ATTENDACE
+             
+             2.ADDING NEW STUDENT (NAME/CLASS/SECTION/ADMISSION 
+               NUMBER FOLLOWING DATA SHOULD ENTER TOGETHER)
+             
+             3.UPDATING NAME
+             
+             4.UPDATING CLASS
+             
+             5.UPDATING SECTION
+             
+             6.UPDATING ADMISSION NUMBER
+             
+             7.TO DISPLAY ATTENDENCE OF A PARTICULAR STUDENT 
+               (ADMN.IS REQUIRED)
+               
+             8.DELETING DATA OF STUDENT
+             
+             9.TO DISPLAY ALL DATA
+             
+             10.EXIT
+           _____________________________________________________  ''')
+             
+             
+ch=eval(input("ENTER THE NUMBER OF OPERATION YOU WANT TO DO :- "))
+
