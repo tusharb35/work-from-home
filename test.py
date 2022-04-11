6247d2dcdff5445654edddc9f2fc8d044626f9a6
Tushar <computerhp80tushar@gmail.com>
2022-04-11 23:51:25 +0530

    last commit of 11-04-2022

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import mysql.connector as conn
 mycon=conn.connect(host='localhost', user='root', passwd='tushar', database='project')
 
-if mycon.is_connected():
-    print("successfully connected to mysql database")
+# if mycon.is_connected():
+#     print("successfully connected to mysql database")
 
 
 cursor=mycon.cursor()
@@ -57,4 +57,58 @@ print('''
              
              
 ch=eval(input("ENTER THE NUMBER OF OPERATION YOU WANT TO DO :- "))
+
+
+
+#==========1.ATTENDANCE==================================
+
+
+
+if ch==1:
+    print('''           
+                        ATTENDENCE                   ''')
+    a=eval(input("ENTER THE CLASS :- "))
+    s=input("ENTER THE SECTION :- ")
+    st="select ROLLNO,NAME from student1 where class={} and sec='{}'".format(a,s)
+    cursor.execute(st)
+    data=cursor.fetchall()
+    for I in data:
+        print(I)
+            
+            
+    count=cursor.rowcount
+    for i in range(1,count):
+        att=input("Enter P if present and A for adsent FOR ROLL NUMBER "+str(i)+" :- ")
+        if att=='P':
+            st1="update student2,student1 set attendance=attendance+1 where sec='{}' and class={} and rollno={} and student1.admno=student2.admno".format(s,a,i)
+            cursor.execute(st1)
+            cursor.execute("select curdate()")
+        else:
+            st2="update student2,student1 set attendance=attendance+0 where sec='{}' and class={} and rollno={} and student1.admno=student2.admno".format(s,a,i)
+            cursor.execute(st2)
+            
+        print('NO ROLL NUMBER ARE LEFT')
+        
+        
+        
+
+#================2.ADDING NEW STUDENT=================
+        
+        
+if ch==2:
+    print('''
+                 ADDING NEW STUDENT           ''' )
+    name=input("ENTER THE NAME :- ")
+    adno=eval(input('ENTER THE ADMISSION NUMBER :- '))
+    clas=eval(input('ENTER THE CLASS :- '))
+    sec=input('ENTER THE SECTION :- ')
+    rn=eval(input('ENTER THE ROLL NUMBER :- '))
+    attendence=0
+    st="insert into student1 values({},'{}',{},'{}',{})".format(adno,name,clas,sec,rn)
+    cursor.execute(st)
+    mycon.commit()
+    st1="insert into student2 values({},{})".format(adno,attendence)
+    cursor.execute(st1)
+    mycon.commit()
+
 
